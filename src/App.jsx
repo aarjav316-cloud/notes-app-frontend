33107841a57142384a5149ff8ef9561e1586601d
Aarjav jain <aarjav316@gmail.com>
2026-02-02 00:23:16 +0530

    frontend foundation: routing and axios setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
+    <>
       
-    </div>
+      <BrowserRouter>
+         <Routes>
+            <Route path='/' element={<h1>Home</h1>} />
+         </Routes>
+      </BrowserRouter>
+
+    </>
   )
 }
 
