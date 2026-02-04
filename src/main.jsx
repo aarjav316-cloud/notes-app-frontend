f5b33efa1adb35376254be40509df393d459c2c5
Aarjav jain <aarjav316@gmail.com>
2026-02-04 23:52:06 +0530

    Wrap app with NotesProvider

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import { AuthProvider } from "./context/AuthContext.jsx";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { NotesProvider } from "./context/NotesContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
+      <NotesProvider>
       <App />
+      </NotesProvider>
     </AuthProvider>
   </React.StrictMode>
 );
